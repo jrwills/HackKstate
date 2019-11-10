40ecc6912802932e32876db7e347f5bc8888d16d
John Wills <jrwills@ksu.edu>
2019-11-09 20:58:34 -0600

    partial, almost done!

--- Controllers/SmsController.cs
+++ Controllers/SmsController.cs
@@ -8,6 +8,7 @@ using Twilio.AspNet.Common;
 using Twilio.AspNet.Mvc;
 using Twilio.TwiML;
 using System.IO;
+using System.Text;
 //--------------------------------------
 namespace WebApplication1.Controllers
 {
@@ -16,39 +17,36 @@ namespace WebApplication1.Controllers
         public TwiMLResult Index(SmsRequest incomingMessage)
         {
             var messagingResponse = new MessagingResponse();
-            string fileName = @"C:\Users\John Wills\Desktop\fileUserInfo.txt";
-            string url;
+            string fileName = @"C:\Users\John Wills\Desktop\testFile.txt";
+            //string url = incomingMessage.Body;
             
-            if (incomingMessage.Body.Length > 5)
-                url = incomingMessage.Body.Substring(5);
+            /*if (incomingMessage.Body.Length > 5)
+            {
+                url = incomingMessage.Body;         //was for determinging what the URL was
+                //Console.WriteLine(url);
+            }    
             else
-                url = null;
+                url = null;*/
 
-            string[] splitEd = incomingMessage.Body.Split(' ');
+            string[] splitEd = incomingMessage.Body.Split(' '); 
             if(splitEd[0] == "!get")
             {
-                /*messagingResponse.Message("The URL you texted was: " +
-                                          incomingMessage.Body.Substring(5) + " Is that correct?(Y+++++/N-----)");*/
-                if (GetUsername(fileName, url) != null || GetPassword(fileName, url) != null)
+                if (GetUserPassword(fileName, splitEd[1]) != null )
                 {
-                    messagingResponse.Message("\nYour " + GetUsername(fileName, url) + "\nYour " + GetPassword(fileName, url));
-                }
-                else
-                {
-                    messagingResponse.Message("URL Not Found..." + "\nTry again with the !get command to find your information");
+                    messagingResponse.Message("\nYour " + GetUserPassword(fileName, splitEd[1]));
                 }
 
             }
-            else if(splitEd[0] == "!help")
+            else if(incomingMessage.Body == "!help")
             {
                 messagingResponse.Message("Text !get (url name) to find your information" +
                         "\nText !contains (url name) to find out if we have that information");
             }
-            else if(splitEd[0] == "!contains" + url)
+            else if(splitEd[0] == "!contains")
             {
-                if (ContainsInFile(fileName, url))
+                if (ContainsInFile(fileName, splitEd[1]))
                 {
-                    messagingResponse.Message("We do have the URL");
+                    messagingResponse.Message("We do have the URL, try the !get command.");
                 }
                 else
                 {
@@ -65,94 +63,36 @@ namespace WebApplication1.Controllers
         }
 
         /// <summary>
-        /// Gets the username
+        /// Gets password and username from file
         /// </summary>
         /// <param name="fileName">name of the file</param>
         /// <returns></returns>
-        public string GetUsername(string fileName, string url)
+        public string GetUserPassword(string fileName, string url)  //WORKS PERFECTLY
         {
-            string newUser;
+            string user;
+            string pass;
+            StreamReader sr = new StreamReader(fileName);
             
-            StreamReader temp = new StreamReader(fileName);
-            int count = 0;
-            while (!temp.EndOfStream)
+            while (!sr.EndOfStream)
             {
-                count++;
-                temp.ReadLine();
-            }
-            temp.Close();
-            //-----Temp is used to find length of the file
-            string[] fileCollect = new string[count];
-            int i = 0;
-            StreamReader sr = new StreamReader(fileName); //using sr to read thru the file
-            while (!sr.EndOfStream) //while file is not fully read
-            {
-                fileCollect[i] = sr.ReadLine();
-                i++;
-            }
-
-            int index = 0;
-
-            for (int j = 0; j < fileCollect.Length; j++)
-            {
-                if (fileCollect[j] != "\n")
+                string test = sr.ReadLine();
+                if (test.Length > 5)
                 {
-                    if (fileCollect[j].Substring(5) == url)  //URL: 5   DID NOT LIKE GMAIL
+                    string[] arr = test.Split(' ');
+                    if (arr[1] == url)
                     {
-                        index = j;
-                        break;
+                        user = sr.ReadLine();
+                        pass = sr.ReadLine();
+                        return user + "" + pass;
                     }
                 }
             }
-            newUser = fileCollect[index + 1];
-            return newUser;
-        }
-
-        /// <summary>
-        /// Gets password from file
-        /// </summary>
-        /// <param name="fileName">name of the file</param>
-        /// <returns></returns>
-        public string GetPassword(string fileName, string url)
-        {
-            string newPass;
+            sr.Close();
+                
             
-            StreamReader temp = new StreamReader(fileName);
-            int count = 0;
-            while (!temp.EndOfStream)
-            {
-                count++;
-                temp.ReadLine();
-            }
-            temp.Close();
-            //-----Temp is used to find length of the file
-            
-            string[] fileCollect = new string[count];
-            int i = 0;
-            StreamReader sr = new StreamReader(fileName); //using sr to read thru the file
-            while (!sr.EndOfStream) //while file is not fully read
-            {
-                fileCollect[i] = sr.ReadLine();
-                i++;
-            }
-
-            int index = 0;
-
-            for (int j = 0; j < fileCollect.Length; j++)
-            {
-                if (fileCollect[j] != "\n")
-                {
-                    if (fileCollect[j].Substring(5) == url)  //URL: 5
-                    {
-                        index = j;
-                        break;
-                    }
-                }
-            }
-            newPass = fileCollect[index + 2];
-            return newPass;
+            return "URL not found";
         }
-
+        
         /// <summary>
         /// Checks to see if the URL you are looking for is in the file
         /// </summary>
@@ -160,54 +100,22 @@ namespace WebApplication1.Controllers
         /// <returns></returns>
         public bool ContainsInFile(string fileName, string url)
         {
-            StreamReader temp = new StreamReader(fileName);
-            int count = 0;
-            while (!temp.EndOfStream)
-            {
-                count++;
-                temp.ReadLine();
-            }
-            temp.Close();
-            //-----Temp is used to find length of the file
+            StreamReader sr = new StreamReader(fileName);
 
-            string[] fileCollect = new string[count];
-
-            StreamReader urlLookup = new StreamReader(fileName);
-            int line = 0;
-            while(!urlLookup.EndOfStream)
+            while (!sr.EndOfStream)
             {
-                if(fileCollect[line].Contains(url))
+                string test = sr.ReadLine();
+                if (test.Length > 5)
                 {
-                    return true;
+                    string[] arr = test.Split(' ');
+                    if (arr[1] == url)
+                    {
+                        return true;
+                    }
                 }
-                line++;
-                urlLookup.ReadLine();
             }
+            sr.Close();
             return false;
         }
     }
 }
-
-
-/*switch (incomingMessage.Body)
-            {
-                default:
-                    messagingResponse.Message("Text a !get (url name) to find your USER and PASS" +
-                        "\nText !help for a list of commands");
-                    break;
-                case "Y":
-                    messagingResponse.Message("\nYour " + GetUsername(fileName, url) + "\nYour " + GetPassword(fileName, url));
-                    break;
-                case "N":
-                    messagingResponse.Message("Text the URL containing the USER and PASSWORD you're looking for");
-                    break;
-                case "!help":
-                    messagingResponse.Message("Text !get (url name) to find your information" +
-                        "\nText !set (url name) to set a new username and password for a site");
-                    break;
-                case "!get " + url:
-                    messagingResponse.Message("The URL you texted was: " +
-                                          incomingMessage.Body.Substring(5) + " Is that correct?(Y/N)");
-                    break;
-
-            }*/
